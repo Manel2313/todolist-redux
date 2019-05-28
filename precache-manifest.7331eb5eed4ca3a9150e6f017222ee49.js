fed2bf42458591b1bdaa6967ccfa04b1e288d03c
Manel2313 <djemal.manel@gmail.com>
2019-05-28 10:09:45 +0100

    Updates

---
+++ precache-manifest.7331eb5eed4ca3a9150e6f017222ee49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7919f51bf5c4db9a10026924b8326a79",
+    "url": "/todolist-redux/index.html"
+  },
+  {
+    "revision": "3e3991a61cab026dba39",
+    "url": "/todolist-redux/static/css/main.95373454.chunk.css"
+  },
+  {
+    "revision": "56ae26d6a4c5cb4c7dd1",
+    "url": "/todolist-redux/static/js/2.ffc642a5.chunk.js"
+  },
+  {
+    "revision": "3e3991a61cab026dba39",
+    "url": "/todolist-redux/static/js/main.988c3be5.chunk.js"
+  },
+  {
+    "revision": "646c25423f063a20bfe7",
+    "url": "/todolist-redux/static/js/runtime~main.513afd38.js"
+  }
+]);
